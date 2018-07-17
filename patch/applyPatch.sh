c9f92f24d8d53b567afb2cc98eaebf6dc298963d
Mingze Xiao <mxiao@box.com>
2018-07-16 17:47:42 -0700

    Fix: Delete Patch

--- patch/applyPatch.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-echo -e '\033[0;36mStart applying the patch...\033[0m'
-echo -e '\033[0;34mPatching SheetJS...\033[0m'
-rm -rf ../node_modules/xlsx
-cp -R ./xlsx ../node_modules
-echo -e '\033[0;32mDone.\033[0m'
